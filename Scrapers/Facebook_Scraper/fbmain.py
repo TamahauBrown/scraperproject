e1447431cf959ee517b25bb89b93f0de2e9d495c
Rose Pineau <rosepineau@protonmail.com>
2022-06-09 12:04:07 -0400

    Finished putting data into dataframes. Experimented w/ concatenating dataframes (see test.py).

--- Scrapers/Facebook_Scraper/fbmain.py
+++ Scrapers/Facebook_Scraper/fbmain.py
@@ -1,21 +1,37 @@
 from facebook_scraper import get_posts
 import pandas as pd
-#import csv
 
 if __name__ == '__main__':
-    # Make Dataframe for Post ID
-    post_id = pd.DataFrame(columns = ["post_id"])
-    
+    # Make Dataframes for Post Attributes
+    post_id = pd.DataFrame(columns = ['post_id'])
+    post_time = pd.DataFrame(columns = ['time'])
+    post_user = pd.DataFrame(columns = ['username'])
+    post_likes = pd.DataFrame(columns = ['likes'])
+    post_comments = pd.DataFrame(columns = ['comments'])
+    post_shares = pd.DataFrame(columns = ['shares'])
+    post_text = pd.DataFrame(columns = ['text'])
+  
     i=0
 
     # For loop looping through posts
-    for post in get_posts('hershey', pages=3):
+    for post in get_posts('nintendo', pages=3):
 
-        #For each position [i] place post_id[i] into i position in dataframe
+        # For each position [i] place post_attribute[i] into 'i' position in dataframe
         post_id.loc[i] = post['post_id']
-        print(post_id)
+        post_time.loc[i] = post['time']
+        post_user.loc[i] = post['username']
+        post_likes.loc[i] = post['likes']
+        post_comments.loc[i] = post['comments']
+        post_shares.loc[i] = post['shares']
+        post_text.loc[i] = post['text']
         
         i+=1
 
-    # Pandas puts dataframe into CSV
-    post_id.to_csv("Post_IDs.csv", index=True)
+    # Pandas puts dataframes into separate CSVs
+    post_id.to_csv('Post_IDs.csv', index = True)
+    post_text.to_csv('Post_Text.csv', index = True)
+    post_time.to_csv('Post_Time.csv', index = True)
+    post_user.to_csv('Post_User.csv', index = True)
+    post_likes.to_csv('Post_Likes.csv', index = True)
+    post_comments.to_csv('Post_Comments.csv', index = True)
+    post_shares.to_csv('Post_Shares.csv', index = True)
