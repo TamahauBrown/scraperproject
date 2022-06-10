95e2a3c7947df1270b0441b3708af51933be9aaa
Tamahau Brown <48704237+TamahauBrown@users.noreply.github.com>
2022-06-11 10:49:15 +1200

    Revert "Twitter API Connection and Pandas DF implemented"
    This reverts commit 0aeddd14d5291725266582c62aac55cad8570979.

--- Scrapers/Scraper.py
+++ Scrapers/Scraper.py
@@ -1,29 +1,18 @@
 from asyncio.windows_events import NULL
 import praw
 import os
-import Tweet_Scraper as ts
 
 class Scraper:
-    #All information is gathered through the system environment variables under the name
     def __init__(self):
-        #Reddit authentication credentials
-        reddit = praw.Reddit(   client_id           =       os.getenv("REDDIT_CLIENT_ID"), 
-                                client_secret       =       os.getenv("REDDIT_CLIENT_SECRET"),
-                                user_agent          =       os.gentenv("REDDUT_USER_AGENT")
-                                )
+        reddit = praw.Reddit(   client_id =     os.getenv("REDDIT_CLIENT_ID"), 
+                                client_secret = os.getenv("REDDIT_CLIENT_SECRET"),
+                                user_agent =    os.gentenv("REDDUT_USER_AGENT"))
         
-        #Twitter authentication credentials
-        twitter = ts.auth(      os.getenv("TWITTER_API_KEY"),
-                                os.getenv("TWITTER_API_KEY_SECRET"),
-                                os.getenv("TWITTER_ACCESS_TOKEN"),
-                                os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-                                )
+        twitter = os.getenv('TWITTER_BEARER_TOKEN')
         facebook = NULL
 
-#Gets credential information for Reddit
 def getReddit(scraper:Scraper()):
     scraper.reddit
 
-#Gets credential information for Twitter
 def getTwitter(scraper:Scraper()):
     scraper.twitter
